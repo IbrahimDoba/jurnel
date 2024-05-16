cde7bfb8cf54cb675ec819854ade3280fbf23172
Ibrahim doba <ibrahimdoba55@gmail.com>
2024-05-16 22:44:43 +0100

    UI: login/signup ui, loadingspinner added

---
+++ src/components/loadSpinner.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LoadSpinner = () => {
+  return (
+    <div className="lds-default">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadSpinner;
