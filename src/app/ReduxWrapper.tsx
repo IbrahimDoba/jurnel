f586d22a233b4a3ddfaf0dd1846d484e5f517135
unknown <daudakolo16@gmail.com>
2024-06-20 06:16:08 +0100

    vercel error

--- src/app/ReduxWrapper.tsx
+++ src/app/ReduxWrapper.tsx
@@ -1,15 +1,8 @@
-"use client";
 import { firebaseAnalytics } from "@/firebase";
 import { ReduxProvider } from "@/redux/provider";
-import { logEvent } from "firebase/analytics";
-import React, { useEffect } from "react";
+import React from "react";
 
 const ReduxWrapper = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      logEvent(firebaseAnalytics, "page_visit");
-    }
-  }, []);
   return (
     <html lang="en">
       <ReduxProvider>{children}</ReduxProvider>
